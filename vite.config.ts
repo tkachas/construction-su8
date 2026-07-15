c033136a027eb2d0c5ee8edab64eecbeab7b4a04
Artem Tkachenko <as.tkachenko@etpgpb.ru>
2026-07-15 18:00:36 +0300

    fix: gitignore for deploy branch

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/construction-su8/" : "/",
+  base: "/",
   plugins: [react()],
   server: {
     host: "127.0.0.1",
